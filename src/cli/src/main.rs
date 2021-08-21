895e2d99b6c4b1d2e11c8a060ea1997a271bf6a7
Rodrigo Moya <rodrigo@gnome.org>
2021-08-21 19:05:32 +0200

    Add basic command line parsing

--- src/cli/src/main.rs
+++ src/cli/src/main.rs
@@ -1,3 +1,17 @@
+use clap::{App, crate_name, crate_version, crate_authors, crate_description, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        //.arg(Arg::with_name("config").short("c").value_name("FILE").takes_value(true))
+        .subcommand(SubCommand::with_name("init")
+            .about("Initialize cluster"))
+        .subcommand(SubCommand::with_name("cleanup"))
+        .get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("init") {
+    } else if let Some(ref matches) = matches.subcommand_matches("cleanup") {
+    }
 }
