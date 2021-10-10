e62f2fa0cdf774062fa7ddabfab3f7f5eade320f
Rodrigo Moya <rodrigo@gnome.org>
2021-10-10 19:08:51 +0200

    Add basic tests for arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 /*
  * Raspberry Pi cluster manager.
  *
- * Copyright (C) 2020-2021 Rodrigo Moya <rodrigo@gnome.org>
+ * Copyright (C) 2020-2021 Rodrigo Moy
+
+    use crate::utils::logging::LogLevel;
+    use crate::utils::logging::LogLevel;
+a <rodrigo@gnome.org>
  */
 
 use clap::{Clap, crate_version, crate_authors, crate_description};
@@ -48,4 +52,33 @@ fn main()
     }
 
     std::process::exit(exit_code);
+}
+
+#[cfg(test)]
+mod tests
+{
+    use clap::Clap;
+    use rstest::rstest;
+    use crate::ClusterSettings;
+    use crate::utils::logging::LogLevel;
+
+    const INVENTORY_FILE: &str = "/tmp/inventory.yaml";
+
+    #[rstest]
+    #[case("-v", LogLevel::Debug)]
+    #[case("-vv", LogLevel::Trace)]
+    #[case("-vvv", LogLevel::Trace)]
+    fn global_settings_are_correctly_parsed(
+        #[case] verbosity_arg: &str,
+        #[case] expected_verbosity: LogLevel)
+    {
+        let settings: ClusterSettings = ClusterSettings::try_parse_from(
+            vec!["pi-cluster", verbosity_arg, "--inventory", INVENTORY_FILE, "update"]
+        ).unwrap();
+
+        assert_eq!(settings.inventory, INVENTORY_FILE);
+
+        let verbosity_level = settings.verbosity_level();
+        matches!(verbosity_level, expected_verbosity);
+    }
 }
